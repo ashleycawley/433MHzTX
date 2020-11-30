ebb78938d2fb23efb79ae57fbe12e901de8c1430
Ashley Cawley <ash@ashleycawley.co.uk>
2020-11-30 20:09:28 +0000

    Going well

--- 433MHzTX.sh
+++ 433MHzTX.sh
@@ -3,7 +3,7 @@
 # Notes
 
 # Command Structure:
-# 433MHzTX.sh --code=4543795 --protocol=0 --pulse-width=170 --tx-number=5 --gaps=0.2
+# 433MHzTX.sh --code=4543795 --protocol=0 --pulse-width=170 --tx-number=5 --gaps=0.2 --device=monitors
 
 # Records start time
 start=`date +%s`
@@ -16,6 +16,8 @@ PROTOCOL=$(echo $2 | sed -n -e 's/^.*=//p')
 PULSE_WIDTH=$(echo $3 | sed -n -e 's/^.*=//p')
 TX_NUMBER=$(echo $4 | sed -n -e 's/^.*=//p')
 GAPS=$(echo $5 | sed -n -e 's/^.*=//p')
+DEVICE=$(echo $6 | sed -n -e 's/^.*=//p')
+COUNTER="1"
 
 echo "codesend $CODE $PROTOCOL $PULSE_WIDTH"
 
@@ -24,7 +26,9 @@ do
 	echo "Running $COUNTER"
 	((COUNTER++))
 done
-	echo "All done"
+
+((COUNTER--)) # Corrects Counter
+echo "`date` - $COUNTER signals of the code $CODE were transmitted to the $DEVICE" >> 433MHz.log
 
 # Records end time
 end=`date +%s`
