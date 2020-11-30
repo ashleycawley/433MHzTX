77397daa9b82c3f5f5cccf89acc563c584379466
Ashley Cawley <ash@ashleycawley.co.uk>
2020-11-30 19:53:20 +0000

    Building vars

--- 433MHzTX.sh
+++ 433MHzTX.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Notes
+
+# Command Structure:
+# 433MHzTX.sh --code=4543795 --protocol=0 --pulse-width=170 --tx-number=5 --gaps=0.2
+
 # Records start time
 start=`date +%s`
 
@@ -7,10 +12,12 @@ start=`date +%s`
 echo "Your arguments were: $1 $2 $3"
 
 CODE=$(echo $1 | sed -n -e 's/^.*=//p')
+PROTOCOL=$(echo $2 | sed -n -e 's/^.*=//p')
+PULSE_WIDTH=$(echo $3 | sed -n -e 's/^.*=//p')
+TX_NUMBER=$(echo $4 | sed -n -e 's/^.*=//p')
+GAPS=$(echo $5 | sed -n -e 's/^.*=//p')
 
-echo "Code: $CODE"
-
-ping -c 5 bbc.co.uk
+echo "codesend $CODE $PROTOCOL $PULSE_WIDTH"
 
 # Records end time
 end=`date +%s`
